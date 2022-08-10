cbbd479110879b6e18d8ba2b4c1393c70e5a9f08
MyronovM <101328391+MyronovM@users.noreply.github.com>
2022-08-10 12:40:22 +0300

    Changed(1st variant)

--- csharp-refactoring/Program.cs
+++ csharp-refactoring/Program.cs
@@ -1,90 +1,124 @@
-﻿using System.ComponentModel.Design;
+using System.ComponentModel.Design;
 using System.Text.Json;
 
-public class Program {
 
-	public enum string_code 
-	{
-		tragedy, 
-		comedy, 
-		none
-	}
+public class Program
+{
+    private enum Genre
+    {
+        Tragedy,
+        Comedy,
+        None
+    }
 
-
-	static string_code hashit(String inString) {
-		if (inString.Equals("tragedy")) {
-			return string_code.tragedy;
-		}
-		if (inString.Equals("comedy")) {
-			return string_code.comedy;
-		} else {
-			return string_code.none;
-		}
-	}
-
-	static String statement(JsonDocument invoices, JsonDocument plays) {
-
-		int totalAmount = 0;
-		int volumeCredits = 0;
-
-
-		String result = "Statement for " + invoices.RootElement.EnumerateArray().First().GetProperty("customer").GetString() + " \n";
-		var performances = invoices.RootElement.EnumerateArray().First().GetProperty("performances");
-
-        foreach(var performance in performances.EnumerateArray())
+    static Genre GetGenre(string inString)
+    {
+        switch (inString)
         {
-                    int thisAmount = 0;
+            case "tragedy":
+                return Genre.Tragedy;
+            case "comedy":
+                return Genre.Comedy;
+            default:
+                return Genre.None;
+        }
+    }
 
-                    var play = plays.RootElement.GetProperty(performance.GetProperty("playID").GetString());
+    static void Statement(JsonDocument invoices, JsonDocument plays)
+    {
+        int totalAmount = 0;
+        int volumeCredits = 0;
+      
+        foreach (JsonElement item in invoices.RootElement.EnumerateArray())
+        {
+            try
+            {
+                string result = "";
+                string customer = item.GetProperty("customer").GetString();
+                result = $"Statement for {customer} \n";
+                
+                var performances = item.GetProperty("performances").EnumerateArray();
+                foreach (var performance in performances)
+                {
+                    int thisAmount = 0;
+                    string playId = performance.GetProperty("playID").GetString();
+                    var play = plays.RootElement.GetProperty(playId);
+
+                    string playType = play.GetProperty("type").GetString();
+                    string playName = play.GetProperty("name").GetString();
 
                     int audience = performance.GetProperty("audience").GetInt32();
+                    Genre genre = GetGenre(playType); //  Enum.Parse(typeof(StringCode), playType, true);
 
-                    switch (((string_code)hashit(play.GetProperty("type").GetString()))) {
-                        case string_code.tragedy:
-                            thisAmount = 40000;
-                            if (audience > 30) {
-                                thisAmount += 1000 * (audience - 30);
-                            }
-                            break;
-                        case string_code.comedy:
-                            thisAmount = 30000;
-                            if (audience > 20) {
-                                thisAmount += 10000 + 500 * (audience - 20);
-                            }
-                            thisAmount += 300 * audience;
-                            break;
-                        default:
-                            return "error";
-                    }
-
-                    // add volume credits
-                    volumeCredits += Math.Max(audience - 30, 0);
-
-                    // add extra credit for every ten comedy attendees
-                    if (play.GetProperty("type").GetString() == "comedy")
-                        volumeCredits += (int)Math.Floor((double)audience / 5.0d);
+                    thisAmount = CalculateAmount(genre, audience);
+                    volumeCredits += CalculateExtra(genre, audience);
 
                     // print line for this order
-                    result += " " + play.GetProperty("name").GetString() + ": $" + (thisAmount / 100) + "(" + audience + " seats)\n";
+                    result += $" {playName}: ${(thisAmount / 100)} ({audience} seats) \n";
                     totalAmount += thisAmount;
+                }
+
+                result += $"Amount owed is $ {(totalAmount / 100)} \n";
+                result += $"You earned {volumeCredits} credits \n";
+                Console.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong " + e.Message);
+            }
+        }
+    }
+    static int CalculateAmount(Genre type, int audience)
+    {
+        int thisAmount = 0;
+        switch (type)
+        {
+            case Genre.Tragedy:
+                thisAmount = 40000;
+                if (audience > 30)
+                {
+                    thisAmount += 1000 * (audience - 30);
+                }
+
+                break;
+            case Genre.Comedy:
+                thisAmount = 30000;
+                if (audience > 20)
+                {
+                    thisAmount += 10000 + 500 * (audience - 20);
+                }
+
+                thisAmount += 300 * audience;
+                break;
+            default:
+                return thisAmount;
         }
 
+        return thisAmount;
+    }
 
-		result += "Amount owed is $" + (totalAmount / 100) + "\n";
-		result += "You earned " + volumeCredits + " credits\n";
-		return result;
-	}
-
-	static void Main(string[] args)
+    static int CalculateExtra(Genre type, int audience)
     {
-		var playsFile = File.ReadAllText("plays.json");
-		var invoicesFile = File.ReadAllText(("invoices.json"));
+        // add volume credits
+        int volumeCredits = Math.Max(audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if (type == Genre.Comedy)
+            volumeCredits += (int) Math.Floor((double) audience / 5.0d);
 
-		var plays = JsonDocument.Parse(playsFile);
+        return volumeCredits;
+    }
 
-		var invoices = JsonDocument.Parse(invoicesFile);
+    static void Main(string[] args)
+    {
+        var playsFile =
+            File.ReadAllText("plays.json");
+        var invoicesFile =
+            File.ReadAllText("invoices.json");
 
-		Console.WriteLine(statement(invoices, plays));
-	}
+        var plays = JsonDocument.Parse(playsFile);
+        var invoices = JsonDocument.Parse(invoicesFile);
 
+        Statement(invoices, plays);
+    }
 }
+
